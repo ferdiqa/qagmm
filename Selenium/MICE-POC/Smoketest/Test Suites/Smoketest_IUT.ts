8fcfebe528b6e0ed8a625fa4e79eb41b386d3b11
LAPTOP-MH8HMEMT\Ferdi Güllübag <ferdi.guelluebag@medial.net>
2018-03-02 15:39:31 +0100

    Add Tests to new IUT FOlders

--- Selenium/MICE-POC/Smoketest/Test Suites/Smoketest_IUT.ts
+++ Selenium/MICE-POC/Smoketest/Test Suites/Smoketest_IUT.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Smoketest</name>
+   <name>Smoketest_IUT</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <lastRun>2018-02-26T12:26:35</lastRun>
@@ -15,60 +15,60 @@
       <guid>c0d12ace-5b3f-4ba6-a0e5-f4a795470c52</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/EWS/Landingpage/Verify all Elements</testCaseId>
+      <testCaseId>Test Cases/IUT/EWS/Landingpage/Verify all Elements</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>7827850f-1bd0-40ea-ad4c-edb101628f3d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/EWS/Landingpage/Verify Searchbar</testCaseId>
+      <testCaseId>Test Cases/IUT/EWS/Landingpage/Verify Searchbar</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>7355643d-7e7a-4a50-99cb-c19887aff5d8</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/EWS/Landingpage/Start search from LP</testCaseId>
+      <testCaseId>Test Cases/IUT/EWS/Landingpage/Start search from LP</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>c9238568-cade-4297-abdf-8e028654043e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/EWS/Resultpage/Search Wizard _ perform and validate search</testCaseId>
+      <testCaseId>Test Cases/IUT/EWS/Resultpage/Search Wizard _ perform and validate search</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>9bac7060-a0c4-4779-aa59-d3cecbd650d6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/EWS/Resultpage/Search Wizard _ verify elements present</testCaseId>
+      <testCaseId>Test Cases/IUT/EWS/Resultpage/Search Wizard _ verify elements present</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>eeaa6bc2-820c-4da9-8296-7c90889a9f1f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/EWS/Header/Check Elements in Header</testCaseId>
+      <testCaseId>Test Cases/IUT/EWS/Header/Check Elements in Header</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>3d7716d1-8a53-4809-b9c6-fcb74d34c0f9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/EWS/Detailpage/Verify elements on detailpage</testCaseId>
+      <testCaseId>Test Cases/IUT/EWS/Detailpage/Verify elements on detailpage</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>ec8748aa-a8a2-429e-bc1c-773095eb0402</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/EWS/Buchen/Straight forward booking</testCaseId>
+      <testCaseId>Test Cases/IUT/EWS/Buchen/Straight forward booking</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>54a90ea9-d8c4-46ac-b602-56f28d82a727</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Widgets/Searchwidget/Start search</testCaseId>
+      <testCaseId>Test Cases/IUT/Widgets/Searchwidget/Start search</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>b9f25a15-592f-45bb-8227-179499be0005</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Widgets/Searchwidget/Verify all elements</testCaseId>
+      <testCaseId>Test Cases/IUT/Widgets/Searchwidget/Verify all elements</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
